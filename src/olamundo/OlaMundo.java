23e32e01c3c38a84e92204513163d28e27cafbe6
Lindembergh Madruga <lindemberghferreira@gmail.com>
2021-06-09 08:44:12 -0300

    Mensagem explicativa

---
+++ src/olamundo/OlaMundo.java
@@ -0,0 +1,22 @@
+package olamundo;
+import javax.swing.JOptionPane;
+import java.util.Scanner;
+
+public class OlaMundo {
+
+	public static void main(String args[]) {
+		System.out.println("Olá Mundo!!!");
+		JOptionPane.showMessageDialog(null, "Olá Mundo!!!");
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Digite seu nome: ");
+		String nome = sc.nextLine();
+		System.out.print("Digite sua idade: ");
+		int idade = sc.nextInt();
+		System.out.print("Digite seu salário: ");
+		double salario = sc.nextDouble();
+		JOptionPane.showMessageDialog(null, "Meu nome é: "+nome);
+		JOptionPane.showMessageDialog(null, "Minha idade é: "+idade);
+		JOptionPane.showMessageDialog(null, "Meu salário é: "+salario);
+		sc.close();
+	}
+}
